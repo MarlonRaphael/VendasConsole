05953f090f9581b39d73f311321b00cf5b7c92ea
Marlon Raphael <marlon.raphaael@hotmail.com>
2020-08-17 19:19:35 -0300

    Cadastro e listagem de clientes

---
+++ Cliente.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendasConsole
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime CriadoEm { get; set; }
+
+        public virtual List<Cliente> Clientes { get; set; }
+
+        public Cliente(string Nome, string Cpf)
+        {
+            this.setNome(Nome);
+            this.setCpf(Cpf);
+            this.setCriadoEm();
+        }
+
+        public string getNome()
+        {
+            return this.Nome;
+        }
+
+        public void setNome(string Nome)
+        {
+            this.Nome = Nome;
+        }
+
+        public string getCpf()
+        {
+            return this.Cpf;
+        }
+
+        public void setCpf(string Cpf)
+        {
+            this.Cpf = Cpf;
+        }
+
+        public DateTime getCriadoEm()
+        {
+            return this.CriadoEm;
+        }
+
+        public void setCriadoEm()
+        {
+            this.CriadoEm = DateTime.Now;
+        }
+
+        public void addCliente(Cliente cliente)
+        {
+            this.Clientes.Add(cliente);
+        }
+
+        public bool validaCpf(string cpf)
+        {
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+            switch (cpf)
+            {
+                case "11111111111":
+                    return false;
+                case "22222222222":
+                    return false;
+                case "33333333333":
+                    return false;
+                case "44444444444":
+                    return false;
+                case "55555555555":
+                    return false;
+                case "66666666666":
+                    return false;
+                case "77777777777":
+                    return false;
+                case "88888888888":
+                    return false;
+                case "99999999999":
+                    return false;
+                case "00000000000":
+                    return false;
+            }
+
+            int peso = 10;
+            int soma = 0;
+            int resto;
+            int digito1, digito2;
+
+            for (int i = 0; i < 9; i++)
+            {
+                soma += Convert.ToInt32(cpf[i].ToString()) * peso;
+                peso--;
+            }
+
+            resto = soma % 11;
+
+            if (resto < 2)
+            {
+                digito1 = 0;
+            }
+            else
+            {
+                digito1 = 11 - resto;
+            }
+
+            peso = 11;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                soma += Convert.ToInt32(cpf[i].ToString()) * peso;
+                peso--;
+            }
+
+            resto = soma % 11;
+
+            if (resto < 2)
+            {
+                digito2 = 0;
+            }
+            else
+            {
+                digito2 = 11 - resto;
+            }
+
+            if (digito1 == Convert.ToInt32(cpf[9].ToString())
+                && digito2 == Convert.ToInt32(cpf[10].ToString()))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"NOME: {Nome} | CPF: {Cpf} | Criado em: {CriadoEm}";
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace VendasConsole
 {
@@ -9,8 +12,16 @@ namespace VendasConsole
         {
             int opcao;
 
+            string Nome;
+            string Cpf;
+
+            bool flag = false;
+
+            List<Cliente> clientes = new List<Cliente>();
+
             do
             {
+                Console.Clear();
                 Console.WriteLine("---- APLICAÇÃO DE VENDAS ----\n");
                 Console.WriteLine("1 - Cadastrar cliente");
                 Console.WriteLine("2 - Listar clientes");
@@ -24,13 +35,56 @@ namespace VendasConsole
                 switch (opcao)
                 {
                     case 1:
+
+                        Console.Clear();
                         Console.WriteLine("---- CADASTRAR CLIENTE ----\n");
+
+                        Console.WriteLine("Digite o Nome do cliente:");
+                        Nome = Console.ReadLine();
+
+                        Console.WriteLine("Digite o CPF do cliente:");
+                        Cpf = Console.ReadLine();
+
+                        if (clientes.Count > 0)
+                        {
+                            foreach (Cliente cli in clientes)
+                            {
+                                if (Cpf.Equals(cli.getCpf()))
+                                {
+                                    flag = false;
+                                    Console.Clear();
+                                    Console.WriteLine("###### CPF já cadastrado, informe outro.######\n");
+                                    Console.ReadKey();
+                                }
+                                else
+                                {
+                                    flag = true;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            flag = true;
+                        }
+
+                        if (flag)
+                        {
+                            clientes.Add(new Cliente(Nome, Cpf));
+                            Console.Clear();
+                            Console.WriteLine("###### Cliente cadastrado com sucesso! ######\n");
+                        }
+
                         break;
                     case 2:
+                        Console.Clear();
                         Console.WriteLine("---- LISTAR CLIENTES ----\n");
+
+                        foreach (Cliente cli in clientes)
+                            Console.WriteLine(cli.ToString());
+
                         break;
                     case 0:
-                        Console.WriteLine("---- Saindo... ----\n");
+                        Console.WriteLine("Saindo...\n");
                         break;
                     default:
                         Console.WriteLine("---- OPÇÃO INVÁLIDA! ----\n");
@@ -41,6 +95,11 @@ namespace VendasConsole
                 Console.ReadKey();
 
             } while (opcao != 0);
+        }
+
+        private static bool ValidarCpf(string cpf)
+        {
+            return false;
         }
     }
 }
